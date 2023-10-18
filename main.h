e554b39c2b86c34c88397ca9a00c75e7564de905
Samfield Hawb <samfield4sure@gmail.com>
2023-10-18 00:58:11 +0000

    code update

--- main.h
+++ main.h
@@ -1,7 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdarg.h>
-
+#include <unistd.h>
+#include <stdlib.h>
 /**
   * struct specifiers - Struct specifiers
   * @specifier: The conversion specifier
@@ -15,5 +16,11 @@ typedef struct specifiers
 
 int _putchar(char c);
 int _printf(const char *format, ...);
-
+int printf_char(va_list list);
+int printf_str(va_list list);
+int printf_porc(va_list list);
+int printf_digit(va_list list);
+int printf_reversed(va_list list);
+int aux(int arg);
+int (*get_function(char c))(va_list a);
 #endif /* MAIN */
